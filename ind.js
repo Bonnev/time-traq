dcbdefb4bdda4d204225cf01f957d5be049c5abf
Bonnev <piggov@gmail.com>
2022-03-30 07:42:00 +0300

    batch 2

--- ind.js
+++ ind.js
@@ -1,7 +1,29 @@
-const monitor = require('node-active-window');
- 
-monitor.getActiveWindow((err, window) => {
-	if (!err) {
-		console.log(window); // { app: 'Code', title: 'test.js - node-active-window - Visual Studio Code' }
+const activeWindow = require('active-win');
+
+const getActiveWindow = async () => {
+	console.log(await activeWindow({}));
+	/*
+	{
+		title: 'Unicorns - Google Search',
+		id: 5762,
+		bounds: {
+			x: 0,
+			y: 0,
+			height: 900,
+			width: 1440
+		},
+		owner: {
+			name: 'Google Chrome',
+			processId: 310,
+			bundleId: 'com.google.Chrome',
+			path: '/Applications/Google Chrome.app'
+		},
+		url: 'https://sindresorhus.com/unicorn',
+		memoryUsage: 11015432
 	}
-});
+	*/
+};
+
+setInterval(() => {
+	getActiveWindow();
+}, 1000);
