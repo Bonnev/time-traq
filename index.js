53bb672f99d9ac571d6e3f349042dbc56d3d860e
Bonnev <piggov@gmail.com>
2022-07-09 11:49:00 +0300

    batch 4

--- index.js
+++ index.js
@@ -3,41 +3,53 @@ const moment = require('moment');
 
 const fs = require('fs');
 
-const activeWindows = [];
+let activeWindows = [];
 
 let tempCounter = 0;
 const getActiveWindow = async () => {
 	const window = await activeWindow({});
-	const fullTitle = window.owner.path + '\t' + window.title;
-	
+	const fullTitle = window?.owner?.path + '\t' + window.title;
+
 	const lastWindow = activeWindows[activeWindows.length - 1];
 	const isNewDay = lastWindow && getCurrentTime().diff(lastWindow.endTime, 'days') > 0;
-	console.log('isnewday',isNewDay)
-	console.log(getCurrentTime())
-	tempCounter++;
 
-	if (!lastWindow || lastWindow.fullTitle !== fullTitle) {
-		lastWindow && fs.promises.appendFile('./test.txt', lastWindow.fullTitle + '\t' + lastWindow.startTime.format('HH:mm:SS') + '\t' + lastWindow.endTime.format('HH:mm:SS') + '\n');
+	if (!lastWindow) {
+		activeWindows.push({
+			fullTitle: fullTitle,
+			startTime: getCurrentTime(),
+			endTime: getCurrentTime()
+		});
+	} else if (lastWindow.fullTitle !== fullTitle) {
+		const line = lastWindow.fullTitle + '\t' + lastWindow.startTime.format('HH:mm:SS') + '\t' + lastWindow.endTime.format('HH:mm:SS') + '\n';
+
+		console.log('Switched window. Writing to file: ' + line);
+		lastWindow && fs.promises.appendFile('./test.txt', line);
+
 		activeWindows.push({
 			fullTitle: fullTitle,
 			startTime: getCurrentTime(),
 			endTime: getCurrentTime()
 		});
 	} else if (!isNewDay) {
+		console.log('Still on: ' + fullTitle);
 		lastWindow.endTime = getCurrentTime();
 	}
+	tempCounter++;
 
 	if (isNewDay) {
 		const dateStr = lastWindow.endTime.format('YYYY.MM.DD');
-		fs.promises.rename('./test.txt', `./test-${dateStr}.txt`);
-		activeWindows = [lastWindow];
+		fs.rename('./test.txt', `./test-${dateStr}.txt`, (err) => {
+			console.log(!!err);
+			console.log('erere', err);
+		});
+		activeWindows = [activeWindows[activeWindows.lenth-1]];
 	}
 }
 function getCurrentTime() {
-	console.log(tempCounter)
+	//console.log(tempCounter)
 	if (tempCounter >= 5) {
-		console.log('marti',moment().add(1, 'days'))
-		return moment().add(1, 'days');
+		//console.log('marti',moment().add(tempCounter, 'days'))
+		return moment().add(tempCounter/5, 'days');
 	}
 
 	return moment();
@@ -50,4 +62,4 @@ function getCurrentTime() {
 
 setInterval(() => {
 	getActiveWindow()
-}, 5000);
+}, 2000);
